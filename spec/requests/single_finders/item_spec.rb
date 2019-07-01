af9febe5d6a3d358cf9a336307dbc30fbbf64605
earl-stephens <stephensearl@hotmail.com>
2019-06-30 21:25:55 -0600

    finished item single finder by name

--- spec/requests/single_finders/item_spec.rb
+++ spec/requests/single_finders/item_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe 'item single finder', type: :request do
     expect(results["data"]["attributes"]["unit_price"]).to eq("1.23")
     expect(results["data"]["attributes"]["merchant_id"]).to eq(@item.merchant_id)
   end
+
+  it "finds an item by name" do
+    get "/api/v1/items/find?name=#{@item.name}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"]["id"]).to eq("#{@item.id}")
+    expect(results["data"]["attributes"]["description"]).to eq(@item.description)
+    expect(results["data"]["attributes"]["unit_price"]).to eq("1.23")
+    expect(results["data"]["attributes"]["merchant_id"]).to eq(@item.merchant_id)
+  end
 end
