84a98d613ec004f7f231c7aa3414fc71859096db
earl-stephens <stephensearl@hotmail.com>
2019-06-30 21:24:57 -0600

    finished item single finder by id

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -3,7 +3,6 @@ class InvoiceItemSerializer
   attributes :id, :item_id, :invoice_id, :quantity
 
   attribute :unit_price do |object|
-    # binding.pry
     '%.2f' % (object.unit_price.to_i / 100.0)
   end
 end
---
+++ spec/requests/single_finders/item_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe 'item single finder', type: :request do
+  before :each do
+    merchant = Merchant.create!(name: "Wile E. Coyote")
+    @item = Item.create!(name: "frog",
+                        description: "green",
+                        unit_price: 123,
+                        merchant_id: merchant.id,
+                        created_at: "2012-03-27T14:54:05.000Z",
+                        updated_at: "2012-03-27T14:54:05.000Z")
+  end
+
+  it "finds an item by id" do
+    get "/api/v1/items/find?id=#{@item.id}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"]["id"]).to eq("#{@item.id}")
+    expect(results["data"]["attributes"]["description"]).to eq(@item.description)
+    expect(results["data"]["attributes"]["unit_price"]).to eq("1.23")
+    expect(results["data"]["attributes"]["merchant_id"]).to eq(@item.merchant_id)
+  end
+end
