faae57afb0a5d38c1bf31d905e11e0e21a108965
earl-stephens <stephensearl@hotmail.com>
2019-07-01 01:34:12 -0600

    finished customer multi finder by updated at

--- spec/requests/multi_finders/customer_spec.rb
+++ spec/requests/multi_finders/customer_spec.rb
@@ -63,4 +63,16 @@ RSpec.describe 'customer multi finder', type: :request do
     expect(results["data"][0]["attributes"]["first_name"]).to eq(@customer1.first_name)
     expect(results["data"][1]["attributes"]["last_name"]).to eq(@customer2.last_name)
   end
+
+  it "finds customer by updated_at" do
+    get "/api/v1/customers/find_all?updated_at=#{@customer1.updated_at}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"].count).to eq(2)
+    expect(results["data"][0]["id"]).to eq("#{@customer1.id}")
+    expect(results["data"][0]["attributes"]["first_name"]).to eq(@customer1.first_name)
+    expect(results["data"][1]["attributes"]["last_name"]).to eq(@customer2.last_name)
+  end
 end
