7927e0cf6d58209676f6bac8a46d8a9448cf809c
earl-stephens <stephensearl@hotmail.com>
2019-06-30 19:48:44 -0600

    finished merchant single finders

--- spec/requests/single_finders/merchant_spec.rb
+++ spec/requests/single_finders/merchant_spec.rb
@@ -16,4 +16,34 @@ RSpec.describe 'merchant single finders', type: :request do
     expect(results["data"]["id"]).to eq("#{@merchant1.id}")
     expect(results["data"]["attributes"]["name"]).to eq(@merchant1.name)
   end
+
+  it "finds merchant by name" do
+    get "/api/v1/merchants/find?name=#{@merchant1.name}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"]["id"]).to eq("#{@merchant1.id}")
+    expect(results["data"]["attributes"]["name"]).to eq(@merchant1.name)
+  end
+
+  it "finds merchant by created at date" do
+    get "/api/v1/merchants/find?created_at=#{@merchant1.created_at}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"]["id"]).to eq("#{@merchant1.id}")
+    expect(results["data"]["attributes"]["name"]).to eq(@merchant1.name)
+  end
+
+  it "finds merchant by updated at" do
+    get "/api/v1/merchants/find?updated_at=#{@merchant1.updated_at}"
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results["data"]["id"]).to eq("#{@merchant1.id}")
+    expect(results["data"]["attributes"]["name"]).to eq(@merchant1.name)
+  end
 end
